11558b9ccc7136bf96308a625cf8ec24ea54a8f7
Mircea-Sebastian Turtureanu <90131332+turtureanu@users.noreply.github.com>
2023-12-02 22:56:47 +0100

    add(day-1): solution using TypeScript and Bun

---
+++ day-1/index.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+
+const Solve = (filePath = 'input') => {
+  let sum = 0;
+
+  fs.readFile(filePath, 'utf-8', (err, fileContents) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Break the file contents into lines (\n) and iterate over each line
+    fileContents.split('\n').forEach((line) => {
+      let firstDigit, secondDigit;
+
+      // Iterate over each character of the line
+      for (let i = 0; i < line.length; i++) {
+        const c = line[i];
+
+        // Check if the character is a number using ASCII
+        if (c.charCodeAt(0) >= 48 && c.charCodeAt(0) <= 57) {
+          if (firstDigit === undefined) {
+            firstDigit = parseInt(c);
+          } else {
+            secondDigit = parseInt(c);
+          }
+        }
+      }
+
+      // If there is only one number in the whole line, assign it to `secondDigit` as well
+      if (secondDigit === undefined) {
+        secondDigit = firstDigit;
+      }
+
+      // The line has at least one number (otherwise both digits are `undefined`)
+      if (firstDigit !== undefined) {
+        sum += parseInt(`${firstDigit}${secondDigit}`);
+      }
+    });
+
+    console.log(`Solution: ${sum}`);
+  });
+};
+
+Solve();
